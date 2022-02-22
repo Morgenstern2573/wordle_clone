78c0788fce3ab2b3ed79af7ec1281d1f8ebb39e2
Bosun Akinyemi <azurnwa.p@gmail.com>
2022-02-22 08:22:15 +0100

    keypress detection logic

--- build/script.js
+++ build/script.js
@@ -1,8 +1,11 @@
+const NUMBER_OF_GUESSES = 6
+let guessesRemaining = NUMBER_OF_GUESSES
+
 function initBoard() {
-    let guessNumbers = 6;
+    
     let board = document.getElementById("game-board");
 
-    for (let i = 0; i < guessNumbers; i++) {
+    for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
         let row = document.createElement("div")
         row.className = "letter-row"
         
@@ -11,9 +14,28 @@ function initBoard() {
             box.className = "letter-box"
             row.appendChild(box)
         }
-        
+
         board.appendChild(row)
     }
 }
+
+document.addEventListener("keyup", (e) => {
+    console.log(e.key)
+
+    let pressedKey = String(e.key)
+    if (pressedKey === "Backspace") {
+        console.log("Backspace!")
+        return
+    }
+    
+    let found = pressedKey.match(/[a-z]/g)
+
+    console.log(found, pressedKey)
+    if (!found || found.length > 1) {
+        return
+    } else {
+        console.log("single key!")
+    }
+})
 
 initBoard();
